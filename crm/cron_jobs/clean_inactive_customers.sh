140cb911b408379c549e6ea6ad4afa0890506e3e
Idoko Augustine <linkwithaugustine@gmail.com>
2025-11-23 23:01:19 +0100

    setup Graphql endpoint with first schema and query

--- crm/cron_jobs/clean_inactive_customers.sh
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -4,10 +4,12 @@
 
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
+# Delete inactive customers using Django ORM and get the number of deleted objects
 DELETED_COUNT=$(python3 manage.py shell -c "
-from datetime import datetime, timedelta
-from crm.models import Customer
+from datetime import timedelta
 from django.utils import timezone
+from crm.models import Customer
+
 cutoff_date = timezone.now() - timedelta(days=365)
 deleted, _ = Customer.objects.filter(order__isnull=True, created_at__lte=cutoff_date).delete()
 print(deleted)
